e34b472b7c553cd7e0ff28661e46a1eeb19b6941
Jurita Brunava <jurita.pro@gmail.com>
2025-10-14 22:36:05 +0300

    Added style.css. Temporary included JavaScript part in index.html

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
-const quizForm = documentElementById('quiz-form');
-quizForm.addEventListener('submit', calculateResults);
+const quizForm = document.getElementById('quiz-form'); // Capture form element
+
+quizForm.addEventListener('submit', calculateResults); // Listen for form submission
 
 function calculateResults(event) {
+   
+  event.preventDefault(); // Prevent default form submission behavior
 
-const formData = new FormData(quizForm);
+  const formData = new FormData(quizForm); // Read values from the form
 
-let scores = {
+  // Initialize scores for each vegetable type
+  let scores = {
     "Carrot": 0,
     "Spinach": 0,
     "Potato": 0
-};
-// ---------
-switch(formData.get('q1')) {
-  case Red:
-    scores.Carrot += 1;
-    break;
-  case y:
-    // code block
-    break;
-  default:
-    // code block
+  };
+  // Question 1
+  switch(formData.get('q1')) {
+    case 'Red':
+      scores.Carrot += 1;
+      break;
+    case 'Green':
+      scores.Spinach += 1;
+      break;
+    case 'Yellow':
+      scores.Potato += 1;
+      break;
+  }
+  // Question 2
+  switch(formData.get('q2')) {
+    case 'Coffee':
+      scores.Carrot += 1;
+      break;
+    case 'Tea':
+      scores.Spinach += 1;
+      break;
+    case 'Beer':
+      scores.Potato += 1;
+      break;
+  }
+  // Question 3
+  switch(formData.get('q3')) {
+    case 'Quiet':
+      scores.Carrot += 1;
+      break;
+    case 'GaeNight':
+      scores.Spinach += 1;
+      break;
+    case 'Party':
+      scores.Potato += 1;
+      break;
+  }
+
 }
